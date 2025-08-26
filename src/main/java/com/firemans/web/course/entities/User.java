a32f99b72539bc1a128d862f2a2e36e32dbb1b3f
Arthur Emanuel <arthuremanuel091@gmail.com>
2025-08-26 13:29:53 -0300

    Part 1

--- src/main/java/com/firemans/web/course/entities/User.java
+++ src/main/java/com/firemans/web/course/entities/User.java
@@ -2,18 +2,21 @@ package com.firemans.web.course.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -21,6 +24,8 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
@@ -73,6 +78,9 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 
+	}
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
